300b334435e0ef2c241cd39ee268a4eb3e9f60b4
Jailanie Kaganovsky <jaicapco@uw.edu>
2024-02-23 13:27:54 -0800

    First API call

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App () {
   const [ word, setWord ] = useState( '' );
@@ -11,9 +12,18 @@ function App () {
     e.preventDefault();
     console.log(word);
     // console.log(e.target[0].value);
+    fetch( `https://api.unsplash.com/photos/random/?query=${ word }&client_id=${ UNSPLASH_KEY }` )
+      .then( ( res ) => res.json() )
+      .then( ( data ) => {
+        console.log(data);
+      } )
+      .catch( ( err ) => {
+        console.log(err);
+      })
   }
 
   // console.log( word );
+  // console.log(process.env.REACT_APP_UNSPLASH_KEY);
 
   return (
     <div>
