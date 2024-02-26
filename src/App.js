1384540f1b6e30255647fa0a3833a2ad212a092d
Jailanie Kaganovsky <jaicapco@uw.edu>
2024-02-26 10:09:00 -0800

    Save images in state

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   function handleSearchSubmit(e) {
     e.preventDefault();
     console.log(word);
     // console.log(e.target[0].value);
+
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        setImages([data, ...images]);
+        // console.log(images);
       })
       .catch((err) => {
         console.log(err);
