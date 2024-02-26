3494c9ecfd6689cf95014e06692e60a8aecf6c22
Jailanie Kaganovsky <jaicapco@uw.edu>
2024-02-26 13:27:01 -0800

    Add logo to file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Container, Navbar } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: 'lightgrey',
 };
 
 function Header({ title }) {
   return (
     <Navbar style={navbarStyle} data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        {/* <Navbar.Brand href="/">{title}</Navbar.Brand> */}
+        <Logo atl={ title } style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
